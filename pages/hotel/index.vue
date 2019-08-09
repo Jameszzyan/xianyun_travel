592b5f3f70f4a70a95937790abe582b0526ce96e
wujiehong <544268828@qq.com>
2019-08-09 13:03:05 +0800

    mutiple selected of hotel

---
+++ pages/hotel/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- 酒店列表组件 -->
+    <HotelList :webData="webData" />
+  </div>
+</template>
+
+<script>
+import HotelList from "@/components/hotel/hotelList";
+export default {
+  components: {
+    HotelList
+  },
+  data() {
+    return {
+      webData: {}
+    };
+  },
+
+  methods: {
+    async init() {
+      let { city } = this.$route.query;
+      let result = await this.$axios({
+        url: `hotels?&city=199`,
+        method: "get"
+      });
+      this.webData = result.data;
+      console.log(this.webData);
+    }
+  },
+
+  mounted() {
+    this.init();
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
+
+
